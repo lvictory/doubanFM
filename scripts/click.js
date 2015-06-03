22aa0896cce9bbea541f67f5163e133a7762a6bd
Raistlin <lxs4242@gmail.com>
2015-06-03 22:43:04 +0800

    start project

---
+++ scripts/click.js
@@ -0,0 +1,12 @@
+
+$.ajax({
+	"url": "https://api.douban.com/v2/book/1220562"
+	,"type": "GET"
+	,"dataType": "JSONP"
+	,success: function(resp) {
+		debugger
+	}
+	,error: function(resp) {
+		debugger
+	}
+})
